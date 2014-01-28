cb401d01bc1c76dbf3f69b24709c1a1f1ed6a552
Sumedha <pramods@umich.edu>
2014-01-28 18:34:13 -0500

    argh

--- app/controllers/manual_controller.rb
+++ app/controllers/manual_controller.rb
@@ -7,7 +7,7 @@ class ManualController < ApplicationController
 		require 'json'
 		message = params[:kanye][:text]
 		url = URI.parse('https://api.groupme.com/v3/bots/post')
-		post_args = {'e6eb5f1ad32abbd7016a29f4ae' => params[:bot_id], "text" => "#{message}"}.to_json
+		post_args = {"bot_id" => 'e6eb5f1ad32abbd7016a29f4ae', "text" => "#{message}"}.to_json
 		a = ActiveSupport::JSON.decode(post_args)
 		resp, data = Net::HTTP.post_form(url, a)
 		redirect_to manual_path
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-	bot_id = e6eb5f1ad32abbd7016a29f4ae
 	require 'net/http'
 	url = URI.parse('https://api.groupme.com/v3/bots/post')
-	post_args = {bot_id => params[:bot_id], 'derp' => params[:text]}
+	post_args = {"bot_id" => 'e6eb5f1ad32abbd7016a29f4ae', "text" => "derp"}.to_json
 	if params[:text] = 'hello'
 		resp, data = Net::HTTP.post_form(url, post_args)
 	end
