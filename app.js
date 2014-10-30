2d3c2c3d6dc12a2e966cd1ba90d618426a387b25
Sumedha <pramods@umich.edu>
2014-10-30 10:34:10 -0400

    Error handlers

--- app.js
+++ app.js
@@ -129,66 +129,19 @@ poet.watch(function () {
  * Error Handler.
  */
 
-app.use(errorHandler());
+// app.use(errorHandler());
 
-// app.use(function(req, res, next){
-//   res.status(404);
-  
-//   // respond with html page
-//   if (req.accepts('html')) {
-//     res.render('404', { url: req.url });
-//     return;
-//   }
+// Handle 404
+app.use(function(req, res) {
+    res.status(400);
+   res.render('404.jade', {title: '404: File Not Found'});
+});
 
-//   // respond with json
-//   if (req.accepts('json')) {
-//     res.send({ error: 'Not found' });
-//     return;
-//   }
-
-//   // default to plain-text. send()
-//   res.type('txt').send('Not found');
-// });
-
-// // error-handling middleware, take the same form
-// // as regular middleware, however they require an
-// // arity of 4, aka the signature (err, req, res, next).
-// // when connect has an error, it will invoke ONLY error-handling
-// // middleware.
-
-// // If we were to next() here any remaining non-error-handling
-// // middleware would then be executed, or if we next(err) to
-// // continue passing the error, only error-handling middleware
-// // would remain being executed, however here
-// // we simply respond with an error page.
-
-// app.use(function(err, req, res, next){
-//   // we may use properties of the error object
-//   // here and next(err) appropriately, or if
-//   // we possibly recovered from the error, simply next().
-//   res.status(err.status || 500);
-//   res.render('500', { error: err });
-// });
-
-// app.get('/404', function(req, res, next){
-//   // trigger a 404 since no other middleware
-//   // will match /404 after this one, and we're not
-//   // responding here
-//   next();
-// });
-
-// app.get('/403', function(req, res, next){
-//   // trigger a 403 error
-//   var err = new Error('not allowed!');
-//   err.status = 403;
-//   next(err);
-// });
-
-// app.get('/500', function(req, res, next){
-//   // trigger a generic (500) error
-//   next(new Error('keyboard cat!'));
-// });
-
+// Handle 500
+app.use(function(error, req, res, next) {
+    res.status(500);
+   res.render('500.jade', {title:'500: Internal Server Error', error: error});
+});
 
 /**
  * Sitemap.xml
