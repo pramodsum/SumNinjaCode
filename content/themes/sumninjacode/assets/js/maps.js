48f077b6fdb866baa98aad346435e4bc583880ed
Sumedha <pramods@umich.edu>
2015-03-09 14:46:40 -0400

    added more placeS
    Conflicts:
    	content/themes/sumninjacode/assets/js/maps.js

--- content/themes/sumninjacode/assets/js/maps.js
+++ content/themes/sumninjacode/assets/js/maps.js
@@ -1,10 +1,17 @@
 var locations = [
   ['Bengaluru', "", 12.9667, 77.5667],
-  ['Hampi', "", 15.3350, 76.4620],
-  ['Pondicherry', "", 11.9310, 79.7852],
+  ['Hampi', "http://sumninjacode.io/hampi-express", 15.3350, 76.4620],
+  ['Pondicherry', "http://pramography.herokuapp.com/pondicherry/", 11.9310, 79.7852],
   ['Golden Temple, Sripuram', "", 12.8733, 79.0884],
   ['Goa', "", 15.4989, 73.8278],
-  ['SVYASA Prashanti Kutiram', "", 12.4600, 77.3800]
+  ['SVYASA Prashanti Kutiram', "", 12.4600, 77.3800],
+  ['Puerto Rico', "http://pramography.herokuapp.com/puerto-rico/", 18.4500, -66.1000],
+  ['Miami, FL', "http://pramography.herokuapp.com/miami/", 25.7753, -80.2089],
+  ['Seattle, WA', "http://pramography.herokuapp.com/seattle-wa/", 47.6097, -122.3331],
+  ['San Francisco, CA', "", 37.7833, -122.4167],
+  ['Ann Arbor, MI', "http://pramography.herokuapp.com/ann-arbor/", 42.2814, -83.7483],
+  ['Davis, CA', "http://pramography.herokuapp.com/davis-ca/", 38.5539, -121.7381],
+  ['Cancun, Mexico', "", 21.1606, -86.8475]
 ];
 
 var map = new google.maps.Map(document.getElementById('map'), {
@@ -19,7 +26,7 @@ var marker, i;
 var geocoder = geocoder = new google.maps.Geocoder();;
 var bounds = new google.maps.LatLngBounds();
 
-for (i = 0; i < locations.length; i++) {  
+for (i = 0; i < locations.length; i++) {
   marker = new google.maps.Marker({
     position: new google.maps.LatLng(locations[i][2], locations[i][3]),
     map: map,
