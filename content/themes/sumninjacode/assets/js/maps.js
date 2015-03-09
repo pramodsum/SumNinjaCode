1379bf206ead04a39f054800adef1539b359804d
Sumedha <pramods@umich.edu>
2015-03-09 14:46:40 -0400

    added more placeS

--- content/themes/sumninjacode/assets/js/maps.js
+++ content/themes/sumninjacode/assets/js/maps.js
@@ -1,11 +1,16 @@
 var locations = [
   ['Bengaluru', "", 12.9667, 77.5667],
   ['Hampi', "http://sumninjacode.io/hampi-express", 15.3350, 76.4620],
-  ['Pondicherry', "", 11.9310, 79.7852],
+  ['Pondicherry', "http://pramography.herokuapp.com/pondicherry/", 11.9310, 79.7852],
   ['Golden Temple, Sripuram', "", 12.8733, 79.0884],
   ['Goa', "", 15.4989, 73.8278],
   ['SVYASA Prashanti Kutiram', "", 12.4600, 77.3800],
-  ['Puerto Rico', "", 18.4500, -66.1000]
+  ['Puerto Rico', "http://pramography.herokuapp.com/puerto-rico/", 18.4500, -66.1000],
+  ['Miami, FL', "http://pramography.herokuapp.com/miami/", 25.7753, -80.2089],
+  ['Seattle, WA', "http://pramography.herokuapp.com/seattle-wa/", 47.6097, -122.3331],
+  ['San Francisco, CA', "", 37.7833, -122.4167],
+  ['Ann Arbor, MI', "http://pramography.herokuapp.com/ann-arbor/", 42.2814, -83.7483],
+  ['Davis, CA', "http://pramography.herokuapp.com/davis-ca/", 38.5539, -121.7381],
   ['Cancun, Mexico', "", 21.1606, -86.8475]
 ];
 
